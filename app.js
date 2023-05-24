35fcbd350d95277407ae765fd03ea35a4045440b
Wojciech Zieliński <wojtekz.6776@gmail.com>
2023-05-24 18:44:03 +0200

    Add online database on MongoDB Atlas

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB")
+mongoose.connect("mongodb+srv://user:nqPnCpzyoaWpsJWw@blog.kr5nepp.mongodb.net/?retryWrites=true&w=majority")
 
 const postSchema = new mongoose.Schema({
   title: String,
@@ -68,18 +68,6 @@ app.post("/", function(req, res) {
 
   res.redirect("/");
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
